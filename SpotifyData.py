d8697cd7d828b98c4852d45a5164013e35246432
Pryce Tharpe <tharpep@purdue.edu>
2024-06-15 23:39:19 -0400

    Imported data from spotify and introduced myself to tkinter.

---
+++ SpotifyData.py
@@ -0,0 +1,22 @@
+import sqlite3 as sq
+import csv
+import pandas as pd
+
+conn = sq.connect('SpotifyDatabase.db')
+
+cursor = conn.cursor()
+
+
+spotidata = []
+
+with open('present.csv', 'r', encoding='utf-8') as file:
+    spotifile = csv.reader(file)
+    for row in spotifile:
+        spotidata.append(row)
+
+
+df = pd.DataFrame(spotidata[1:], columns=spotidata[0])
+
+
+
+
